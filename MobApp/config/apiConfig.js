aac70e209e692dfc79ca33c0b84089b44bc2896a
Tarun Ramireddy <tarunramireddy@gmail.com>
2025-04-20 02:09:33 -0400

    Added docker

--- MobApp/config/apiConfig.js
+++ MobApp/config/apiConfig.js
@@ -2,5 +2,5 @@ import { Platform } from 'react-native';
 
 export const API_BASE_URL =
   Platform.OS === 'web'
-    ? 'http://localhost:5000/api'
-    : 'http://192.168.86.89:5000/api';
+    ? process.env.REACT_APP_BACKEND_URL || 'http://localhost:5050/api'
+    : 'http://192.168.86.89:5050/api';
