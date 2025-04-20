3b5d65e0fae8aebc2fcc266f232774879bff3e99
Tarun Ramireddy <tarunramireddy@gmail.com>
2025-04-20 13:30:23 -0400

    Front End Deployment

--- MobApp/config/apiConfig.js
+++ MobApp/config/apiConfig.js
@@ -1,6 +1,8 @@
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
-export const API_BASE_URL =
-  Platform.OS === 'web'
-    ? process.env.REACT_APP_BACKEND_URL || 'http://localhost:5050/api'
-    : 'http://192.168.86.89:5050/api';
+// export const API_BASE_URL =
+//   Platform.OS === 'web'
+//     ? process.env.REACT_APP_BACKEND_URL || 'http://localhost:5050/api'
+//     : 'http://192.168.86.89:5050/api';
+
+export const API_BASE_URL = 'https://techmonprojectnew-s5ad.onrender.com/api';
